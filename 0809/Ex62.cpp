437eacca0a6b6a5dddcc5a4c614737e94e2c9503
Caetra <144840275+CaetraBreves@users.noreply.github.com>
2023-11-08 19:02:37 -0100

    Ex62.cpp
    Corrected

--- 0809/Ex62.cpp
+++ 0809/Ex62.cpp
@@ -3,36 +3,38 @@ using namespace std;
 
 int main()
 {
-    int vet[9], count=0;
+    const int total = 5;
+    int vet[total], count = 0, vetNaoRepetido[total];
+    bool ver = false;
 
-    for (int i = 0; i < 10; i++)
-    {
+        for (int i = 0; i < total; i++)
+        {
         cout << i << " Insira um valor: " << endl;
         cin >> vet[i];
-
-    }
-    cout << "--------------------" << endl;
-    for (int i = 0; i < 10; i++)
-    {
-        count=0;
-        for (int j = 0; j < 10; j++)
-        {
-         if(vet[i] == vet[j]){               
-                count++;  
-                if (count ==2)
-                {
-                vet[i] ={};
-                }
-
-            }
-        
         }
 
-        
-    }
-    for (int i = 0; i < 10; i++)
+        cout << "--------------------" << endl;
+    for (int i = 0; i < total; i++)
     {
-        cout << vet[i] << endl;
-    }
-    
+    bool repetido = false;
+        for (int j = i + 1; j < total && repetido == false; j++)
+        {
+            if (vet[i] == vet[j]) {
+            repetido = true;
+            }
+        }
+
+        if (repetido == false) {
+        vetNaoRepetido[count] = vet[i];
+        count++;
+        }
+
+
+        }
+
+        cout << "Este e o array sem os repetidos: " << endl;
+        for (int i = 0; i < count; i++) {
+        cout << vetNaoRepetido[i] << endl;
+
+        }
 }
